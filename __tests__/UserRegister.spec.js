1426b2a40010bb29aaa602efce97365913fc5bec
frankm <117271+frankm@users.noreply.github.com>
2022-02-02 03:03:37 -0800

    validation-intro

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -12,46 +12,66 @@ beforeEach(() => {
 });
 
 describe('User Registration', () => {
-  const user1 = {
+  const validUser = {
     username: 'user1',
     email: 'user1@mail.com',
     password: 'P4ssword',
   };
-  const postValidUser = () => {
-    return request(app).post('/api/1.0/users').send(user1);
+  const postUser = (user = validUser) => {
+    return request(app).post('/api/1.0/users').send(user);
   };
 
   it('returns 200 OK for valid signup request', async () => {
-    const res = await postValidUser();
+    const res = await postUser();
     expect(res.status).toBe(200);
   });
 
   it('returns success message for valid signup request', async () => {
-    const res = await postValidUser();
+    const res = await postUser();
     expect(res.body.message).toBe('User created');
   });
 
   it('saves user to db', async () => {
-    await postValidUser();
+    await postUser();
 
     const userList = await User.findAll();
     expect(userList.length).toBe(1);
   });
 
   it('saves username & email to db', async () => {
-    await postValidUser();
+    await postUser();
 
     const userList = await User.findAll();
     const savedUser = userList[0];
-    expect(savedUser.username).toBe(user1.username);
-    expect(savedUser.email).toBe(user1.email);
+    expect(savedUser.username).toBe(validUser.username);
+    expect(savedUser.email).toBe(validUser.email);
   });
 
   it('hashes password in db', async () => {
-    await postValidUser();
+    await postUser();
 
     const userList = await User.findAll();
     const savedUser = userList[0];
-    expect(savedUser.password).not.toBe(user1.password);
+    expect(savedUser.password).not.toBe(validUser.password);
+  });
+
+  it('returns 400 when username is null', async () => {
+    const invalidUser = { ...validUser, username: null };
+    const response = await postUser(invalidUser);
+    expect(response.status).toBe(400);
+  });
+
+  it('returns validationErrors field in response body when validation occurs', async () => {
+    const invalidUser = { ...validUser, username: null };
+    const response = await postUser(invalidUser);
+    const body = response.body;
+    expect(body.validationErrors).not.toBeUndefined();
+  });
+
+  it('returns Username cannot be null, when username is null', async () => {
+    const invalidUser = { ...validUser, username: null };
+    const response = await postUser(invalidUser);
+    const body = response.body;
+    expect(body.validationErrors.username).toBe('Username cannot be null');
   });
 });

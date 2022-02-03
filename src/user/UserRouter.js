af15a567287d1ba0a58806eeeec40da88b7883b0
frankm <117271+frankm@users.noreply.github.com>
2022-02-02 20:28:42 -0800

    express-validator

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -1,34 +1,20 @@
 const express = require('express');
 const UserService = require('./UserService');
 const router = express.Router();
-
-const validateUsername = (req, res, next) => {
-  const user = req.body;
-  if (user.username === null) {
-    req.validationErrors = {
-      username: 'Username cannot be null',
-    };
-  }
-  next();
-};
-
-const validateEmail = (req, res, next) => {
-  const user = req.body;
-  if (user.email === null) {
-    req.validationErrors = {
-      ...req.validationErrors,
-      email: 'Email cannot be null',
-    };
-  }
-  next();
-};
+const { check, validationResult } = require('express-validator');
 
 // prettier-ignore
-router.post('/api/1.0/users', validateUsername, validateEmail, async (req, res) => {
-  if (req.validationErrors) {
-    const response = {validationErrors: {...req.validationErrors}};
-    return res.status(400).send(response)
-  }
+router.post('/api/1.0/users',
+  check('username').notEmpty().withMessage('Username cannot be null'),
+  check('email').notEmpty().withMessage('Email cannot be null'),
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log('errors =', errors);
+    if (!errors.isEmpty()) {
+      const validationErrors = {};
+      errors.array().forEach((error) => (validationErrors[error.param] = error.msg));
+      return res.status(400).send({ validationErrors: validationErrors });
+    }
     await UserService.save(req.body);
     return res.send({ message: 'User created' });
   }

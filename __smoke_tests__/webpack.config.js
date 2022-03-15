6d0012da1cd28ec8b39a5786e0bd9058e81f86db
frankm <117271+frankm@users.noreply.github.com>
2022-03-14 23:07:58 -0700

    config-webpack-for-heroku

--- __smoke_tests__/webpack.config.js
+++ __smoke_tests__/webpack.config.js
@@ -2,11 +2,12 @@ const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 const nodeExternals = require('webpack-node-externals');
 
 const path = require('path');
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
   target: 'node', // in order to ignore built-in modules like path, fs, etc.
   externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
-  mode: 'development',
+  mode: isProduction ? 'production' : 'development',
 
   entry: './index.js',
   output: {

cc8ed18deffe201e8992bdbe20bdd8fefac5aa55
frankm <117271+frankm@users.noreply.github.com>
2022-03-20 18:29:04 -0700

    add host to listening log

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const app = require('./src/app');
 const sequelize = require('./src/config/dbInstance');
 const TokenService = require('./src/auth/TokenService');
 const logger = require('./src/shared/logger');
+const host = require('ip').address();
 
 sequelize.sync();
 
 TokenService.scheduleCleanup();
 
 const port = parseInt(process.env.PORT) || 3000;
-app.listen(port, () => logger.info('app is running on port:' + port + '. verson: ' + process.env.npm_package_version));
+app.listen(port, () => {
+  logger.info(`app running on http://${host}:${port} verson: ${process.env.npm_package_version}`);
+});

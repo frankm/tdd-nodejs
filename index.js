e030d1ef3fe8606999d2e56d4892c9939781ae22
frankm <117271+frankm@users.noreply.github.com>
2022-03-20 20:16:00 -0700

    1505-fix-yml-directory

--- index.js
+++ index.js
@@ -8,4 +8,6 @@ sequelize.sync();
 TokenService.scheduleCleanup();
 
 const port = parseInt(process.env.PORT) || 3000;
-app.listen(port, () => logger.info('app is running on port:' + port + '. verson: ' + process.env.npm_package_version));
+app.listen(port, () => {
+  logger.info(`app is running on port:${port}. version: ${process.env.npm_package_version}`);
+});
